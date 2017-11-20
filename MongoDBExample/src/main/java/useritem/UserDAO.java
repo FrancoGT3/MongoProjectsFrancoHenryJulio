dc5d533e98e78d692bcb4078e1a5c8d4f83eaee6
DarkFranco <darkfranco@github.com>
2017-11-20 00:43:23 -0500

    Ejemplo de MongoDB
    Hecho por Franco Gutierrez Tamayo, Henry Pinto y Julio Serrano

---
+++ MongoDBExample/src/main/java/useritem/UserDAO.java
@@ -0,0 +1,18 @@
+package useritem;
+
+import java.util.List;
+
+public interface UserDAO 
+{
+
+	void save(User user);
+
+	void save(List<User> users);
+
+	User getById(String id);
+
+	long getUserCount();
+
+	List<ItemCount> getTopTenItems();
+
+}
